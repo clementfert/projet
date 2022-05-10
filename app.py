96fd02712c29a469c39a32de73660af04fb55510
clement fert <clementfert@hotmail.fr>
2022-05-10 16:49:56 +0100

    back-end ajout crypto okay

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
-from flask import Flask, render_template
-from bitcoin import devise, prix, tableau
+from flask import Flask, render_template, request
+from bitcoin import recherche
+
+
+#from requests import request
+#from bitcoin import devise, prix, tableau
 
 app = Flask(__name__)
 
-
-
 @app.route('/')	
+def add():
+ return  render_template('add.html')
 
-def addpage():
-    return render_template('add.html', crypto = devise, prix_unitaire = prix, choose = tableau )
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    # on recupere les valeur entrer par l'user 
+    global crypto_selectionner
+    crypto_selectionner = request.form['crypto_selectionner']
+    global quantity
+    quantity = request.form['quantity']
+    
+    # on convertie le valeur rentré par l'utilisateur en float  
+    try:
+        quantity= float(quantity)
+    except:
+        return render_template('add.html')
+
+    # on verifie si la valeur est bien quantité est bien en float et on lance la recherche sinon on refrech la page  
+    if  quantity == float(quantity):
+
+       recherche()
+       from bitcoin import info, prix_total
+       while info == False: 
+         return render_template('add.html') 
+       if info == True:
+         return render_template('add_final.html', prix_total = prix_total, quantity = quantity, crypto_selectionner = crypto_selectionner)
+
+    else:
+        return render_template('add.html')
+
+     
+
+
+    
--- bitcoin.py
+++ bitcoin.py
@@ -1,43 +1,60 @@
 
-import apikey
-import requests
-
-headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': apikey.key,
-}
-
-params = {
-  'start':'1',
-  'limit':'5',
-  'convert':'USD'
-}
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-json = requests.get(url,params=params,headers=headers).json()
 
 
+def recherche():
+  import apikey
+  from flask import render_template
+  import requests
+  from app import crypto_selectionner, quantity
+  global prix_total
+  global info
+
+  headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': apikey.key,
+  }
+
+  params = {
+    'start':'1',
+    'limit':'5',
+    'convert':'USD'
+  }
+  url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+  json = requests.get(url,params=params,headers=headers).json()
 
 
-coins = json['data']
-choice = input("selectioner votre crypto  ")
-valeur_indice =0
-tableau = []
-for x in coins:
 
-    print(x['symbol'],x['quote']['USD']['price'])
-    tableau.append(x['symbol']) 
-   
-print (tableau)
+  coins = json['data']
 
-if choice in tableau:
+  valeur_indice =0
+  tableau = []
+  for x in coins:
 
-    for i,e in enumerate(tableau):
-         if e == choice:
-             print (i,e)
-             valeur_indice=i
+      print(x['symbol'],x['quote']['USD']['price'])
+      tableau.append(x['symbol']) 
     
-    devise = coins[valeur_indice]['symbol']
-    prix = coins[valeur_indice]['quote']['USD']['price']
-    print (f"{devise}   {prix}")
-    print()
+  print (tableau)
+
+  if crypto_selectionner in tableau:
+
+      for i,e in enumerate(tableau):
+          if e == crypto_selectionner:
+              print (i,e)
+              valeur_indice=i
+      
+      devise = coins[valeur_indice]['symbol']
+      prix = coins[valeur_indice]['quote']['USD']['price']
+      prix_total = float(quantity)
+      prix_total = prix * prix_total
+      print (f"{devise}   {prix}")
+      info=True
+      return info
+  else:
+    info = False
+    prix_total = 0
+    return info
+
+
+
+     
 
